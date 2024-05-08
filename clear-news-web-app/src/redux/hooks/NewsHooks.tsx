4a8b7f296dc636361724f67abb9af06ce4cbbd74
NTKdz <nguyenthekhoi2003@gmail.com>
2024-05-08 07:21:49 +0700

    temp

--- clear-news-web-app/src/redux/hooks/NewsHooks.tsx
+++ clear-news-web-app/src/redux/hooks/NewsHooks.tsx
@@ -111,6 +111,7 @@ export default function NewsHooks() {
 
       if (response.status === 200) {
         const data: NewsDetail = response.data[id].info;
+        getAudio(data.body);
         dispatch(setNewsDetail(data));
       }
     } catch (error) {
@@ -270,6 +271,60 @@ export default function NewsHooks() {
     }
   }
 
+  async function getAudio(text: string) {
+    const key = [
+      "8e8798413dmshf57c00d04dfa197p1e8da5jsnaadb5e702399",
+      "0917e38b6fmsh25dbcb67d26c191p1e2df3jsnc92302097b33",
+      "f6b1f07fbcmshb9a187811f54d80p1a0023jsnb383338ae2de",
+    ];
+
+    const options = {
+      method: "POST",
+      url: "https://realistic-text-to-speech.p.rapidapi.com/v3/generate_voice_over_v2",
+      headers: {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": key[2],
+        "X-RapidAPI-Host": "realistic-text-to-speech.p.rapidapi.com",
+      },
+      data: {
+        voice_obj: {
+          id: 2014,
+          voice_id: "en-US-Neural2-A",
+          gender: "Male",
+          language_code: "en-US",
+          language_name: "US English",
+          voice_name: "John",
+          status: 2,
+          rank: 0,
+          type: "google_tts",
+          isPlaying: false,
+        },
+        json_data: [
+          {
+            block_index: 0,
+            text: text,
+          },
+        ],
+      },
+    };
+
+    try {
+      const response = await axios.request(options);
+
+      if (response.status === 200) {
+        const data: {
+          link: string;
+          block_index: number;
+          duration: number;
+          size: number;
+        } = response.data;
+        console.log(data);
+        return data.link;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
   return {
     getNewsBySearch,
     getNewsDetail,
